d605a144008f3a0b616a97d3f4751051dbb054d9
holla-chengguoshuai <guoshuai.cheng@holla.world>
2024-02-22 18:38:29 +0800

    realm Demo

---
+++ realm-swift-iOS/ExploreRealm/UserTableViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  UserTableViewCell.swift
+//  ExploreRealm
+//
+//  Created by Xcode on 19/2/24
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var userLabel: UILabel!
+    
+    var editClosure: (() -> Void)?
+    var deleteClosure: (() -> Void)?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func populateData(user: User) {
+        _ = user.id
+        let firstName = user.firstName
+        let lastName = user.lastName
+        let age = user.age
+        let fullName = firstName + " " + (lastName )
+        
+        userLabel.text = "\(fullName) is \(age) years old!"
+    }
+    
+    @IBAction func editBtn(_ sender: Any) {
+        editClosure?()
+    }
+    @IBAction func deleteBtn(_ sender: Any) {
+        deleteClosure?()
+    }
+}
